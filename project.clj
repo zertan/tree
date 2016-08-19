8075869e75a712bfeb0d0aa5a4c22140ad14b714
Daniel Hermansson <daniel@murrays.nu>
2016-08-19 22:03:54 +0200

    Init commit.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject trie "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [criterium "0.4.4"]]
+  :main ^:skip-aot trie.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :jvm-opts ["-Dcom.sun.management.jmxremote"
+             "-Dcom.sun.management.jmxremote.ssl=false"
+             "-Dcom.sun.management.jmxremote.authenticate=false"
+             "-Dcom.sun.management.jmxremote.port=43210"])
